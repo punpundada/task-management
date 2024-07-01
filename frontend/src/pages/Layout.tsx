a17e6acd2e875ab0442bcde3b9d6559e1daf9710
Parashkar prajwal <prajwalparashkar100@gmail.com>
2024-07-02 02:02:52 +0530

    changes in task list page

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -4,8 +4,8 @@ import { Outlet } from "react-router-dom"
 const Layout = () => {
   return (
     <div className="w-screen flex min-h-screen">
-      <div className="w-[6%] h-screen"><SideBar/></div>
-      <div className="w-[94%] h-full">
+      <div className="w-[4%] h-screen hidden md:block"><SideBar/></div>
+      <div className="w-full  md:w-[96%] h-full p-3">
         <Outlet/>
       </div>
     </div>
--- frontend/src/pages/task/TaskList.tsx
+++ frontend/src/pages/task/TaskList.tsx
@@ -1,9 +1,16 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 
 const TaskList = () => {
   return (
-    <div>
-      Task List
-    </div>
+    <Card className="shadow-lg">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">Welcome back!</CardTitle>
+          <CardDescription className="text-base">Here's a list of your tasks for this month!</CardDescription>
+        </CardHeader>
+        <CardContent>
+          HELLO
+        </CardContent>
+    </Card>
   )
 }
 
