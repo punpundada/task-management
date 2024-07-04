b2eb78282ae221c302945571768f4a9e08bac4fc
Parashkar prajwal <prajwalparashkar100@gmail.com>
2024-07-04 23:40:45 +0530

    added the cors polycy to backedn

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,32 @@
-import express from 'express';
-import env from './utils/env';
-import { STATUS_CODES } from './utils/lib';
-import authRouter from './routes/auth';
-import { userRouter } from './routes/user';
-import errorHandler from './middleware/errorHandler';
-import { csrfProtection, validateSession } from './middleware/authMiddleware';
-import taskRouter from './routes/task';
-
+import express from "express";
+import env from "./utils/env";
+import { STATUS_CODES } from "./utils/lib";
+import authRouter from "./routes/auth";
+import { userRouter } from "./routes/user";
+import errorHandler from "./middleware/errorHandler";
+import { csrfProtection, validateSession } from "./middleware/authMiddleware";
+import taskRouter from "./routes/task";
+import cors from "cors";
 const app = express();
 
-app.use(express.urlencoded())
-app.use(express.json())
+app.use(env.NODE_ENV === "development" ? cors() : cors());
+app.use(express.urlencoded());
+app.use(express.json());
 
-app.get("/api/health-check",(req,res)=>{
-    return res.status(STATUS_CODES.OK).json({
-        healthy:true
-    })
-})
+app.get("/api/health-check", (req, res) => {
+  return res.status(STATUS_CODES.OK).json({
+    healthy: true,
+  });
+});
 
-app.use(csrfProtection)
-app.use(validateSession)
-app.use('/api/auth',authRouter)
-app.use('/api/user',userRouter)
-app.use('/api/tasks',taskRouter)
+app.use(csrfProtection);
+app.use(validateSession);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/tasks", taskRouter);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(env.PORT,()=>{
-    console.log(`Server started on ${env.BASE_URL}`)
-})
+app.listen(env.PORT, () => {
+  console.log(`Server started on ${env.BASE_URL}`);
+});
